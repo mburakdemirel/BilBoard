690b194164553600962f27468ad04cec795e8e60
ernarim <hayrettin.arim@ug.bilkent.edu.tr>
2023-10-07 22:19:23 +0300

    -Some html pages deleted.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import ProductDetailPage from "./components/ProductDetailPage";
 function App() {
   return (
     <div className="App">
-      <ProductDetailPage/>
+      <LandingPage/>
     </div>
   );
 }
