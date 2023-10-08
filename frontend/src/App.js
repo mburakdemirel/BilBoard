011814e1ad391139a9662a9164a9bd3864bea34a
ernarim <hayrettin.arim@ug.bilkent.edu.tr>
2023-10-09 02:49:58 +0300

    -Default NavigationBar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,17 +7,18 @@ import ProductDetailPage from "./components/ProductDetailPage";
 import {Route, Routes} from 'react-router-dom';
 import NavigationBarLanding from './components/NavigationBarLanding';
 import Footer from './components/Footer';
+import NavigationBarDefault from "./components/NavigationBarDefault";
 
 // we don't need to add NavigationBar and Footer to each page because they are added here only the Routes part of the app will differ.
 function App() {
   return (
     <div className="App">
-      <NavigationBarLanding></NavigationBarLanding>
+
       <Routes>
-        <Route path='/login' element={<LoginPage></LoginPage>}></Route>
-        <Route path='/register' element={<RegisterPage></RegisterPage>}></Route>
+        <Route path='/login' element={<><NavigationBarLanding/><LoginPage/></>}></Route>
+        <Route path='/register' element={<><NavigationBarLanding/><RegisterPage/></>}></Route>
         {/** We will probably add a new component called <ProtectedRoute> or something for pages that should be seen after authentication */}
-        <Route path='/' element={<LandingPage></LandingPage>}></Route>
+        <Route path='/' element={<><NavigationBarLanding/><LandingPage/></>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ frontend/src/components/NavigationBarDefault.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import Logo from './assets/img/logo_bugbunny-removebg-preview.png'
+import Burak from './assets/img/burak.png'
+import { NavLink } from 'react-router-dom';
+
+const navbarStyle = {
+    borderBottom: '2px solid #2d3647',
+};
+
+const brandIconStyle = {
+    margin: '0px',
+    borderRadius: '10px',
+    background: 'var(--bs-body-bg)',
+    width: '50px',
+    height: '50px',
+};
+
+const brandLogoStyle = {
+    width: '100%',
+    height: '100%',
+    borderRadius: '10px',
+};
+
+function NavigationBarDefault(){
+    return (
+        <nav className="navbar navbar-expand-md sticky-top bg-body py-3" style={navbarStyle}>
+            <div className="container">
+                <a className="navbar-brand d-flex align-items-center" href="#">
+          <span className="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon" style={brandIconStyle}>
+            <img style={brandLogoStyle} src={Logo} alt="Logo" />
+          </span>
+                    <span style={{ fontWeight: 'bold', fontFamily: 'Inter, sans-serif' }}>BilBoard</span>
+                </a>
+                <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-3">
+                    <span className="visually-hidden">Toggle navigation</span>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div id="navcol-3" className="collapse navbar-collapse" style={{ fontFamily: 'Inter, sans-serif' }}>
+                    <ul className="navbar-nav mx-auto" style={{ fontWeight: 'bold' }}>
+                        <li className="nav-item"><a className="nav-link active" href="#" style={{ textDecoration: 'underline' }}>Second-Hand</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#">Lost & Found</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#">Borrow</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#">Donation</a></li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <span style={{ color: 'var(--bs-nav-link-hover-color)' }}>Compliant</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <div className="me-4" style={{ maxWidth: '200px', height: '40px' }}>
+                        <input className="d-flex justify-content-xxl-center" type="search" style={{ width: '100%', height: '100%', borderRadius: '6px', border: '2px solid var(--bs-navbar-active-color)', paddingLeft: '5px', paddingRight: '5px', fontFamily: 'Inter, sans-serif', textAlign: 'center' }} placeholder="Search" />
+                    </div>
+                    <button className="btn btn-primary d-xxl-flex justify-content-xxl-center align-items-xxl-center" type="button" style={{ background: '#2d3648', borderStyle: 'none', height: '40px', width: '90.6875px', padding: '0px', marginTop: '5px', marginBottom: '5px' }}>
+                        Add
+                        <svg className="bi bi-plus-circle" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" style={{ marginLeft: '7px', fontSize: '20px' }}>
+                            <path d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"></path>
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
+                        </svg>
+                    </button>
+                    <svg className="bi bi-bell" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" style={{ fontSize: '30px', marginRight: '15px', marginLeft: '15px' }}>
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"></path>
+                    </svg>
+                    <svg className="bi bi-envelope" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" style={{ fontSize: '30px', marginRight: '15px', marginTop: '5px', marginBottom: '5px' }}>
+                        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"></path>
+                    </svg>
+                    <div><img className="rounded-circle" style={{ width: '50px', height: '50px' }} src={Burak} alt="Profile" /></div>
+                </div>
+            </div>
+        </nav>
+
+
+
+    );
+
+};
+
+export default NavigationBarDefault;
