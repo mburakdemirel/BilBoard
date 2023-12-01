03d79c530618b7291a12858e4ad3eee876bec239
Dilara Mandıracı <dilaramandiracii@gmail.com>
2023-12-02 00:19:56 +0300

    pagination settings added = 16

--- backend/bilboard_backend/settings.py
+++ backend/bilboard_backend/settings.py
@@ -156,7 +156,9 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 16,  # of items per page
 }
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
