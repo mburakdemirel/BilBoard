d9ed8119b3a346615206db53cb6900a0a841e357
Yusuf Toraman <yusuf-toraman@hotmail.com>
2023-12-04 16:39:15 +0300

    description added to user serializer

--- backend/entryapp/serializers.py
+++ backend/entryapp/serializers.py
@@ -4,15 +4,15 @@ from mainapp.models import LostAndFoundEntry, CustomUser, ComplaintEntry
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'profile_photo', 'name', 'surname', 'phone_number']
+        fields = ['id', 'profile_photo', 'name', 'surname', 'phone_number', 'description']
 
-#user olmamalı yeni serializer yarattım o yüzden
+
 class CreateLostAndFoundEntrySerializer(serializers.ModelSerializer):
     class Meta:
         model = LostAndFoundEntry
         fields = ['id', 'topic', 'description', 'upload_date']
 
-#user returns with id
+#2 serializer da aynı oldu güncellemelerle ileride düzelt!!!!
 class DefaultLostAndFoundEntrySerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     class Meta:
@@ -32,7 +32,6 @@ class DefaultComplaintEntrySerializer(serializers.ModelSerializer):
     liste olarak görünümde description göstermeyen serializer
     """
     user = UserSerializer(read_only=True)
-    # user döndürmedik, çünkü sikayetler anonim olsun. Complaint checker check edicek.
     class Meta:
         model = ComplaintEntry
         fields = ['id', 'topic', 'description', 'user', 'upload_date', 'vote', 'target_mail']
