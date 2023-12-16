13b6dc96aa49243a15257b75bba5980628fd3420
Yusuf Toraman <yusuf-toraman@hotmail.com>
2023-12-17 01:39:14 +0300

    rate sorunu

--- backend/productapp/serializers.py
+++ backend/productapp/serializers.py
@@ -6,7 +6,7 @@ from mainapp.helpers import validate_image_safety
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'email', 'name', 'surname', 'profile_photo', 'rate_ratio', 'description', 'phone_number']
+        fields = ['id', 'email', 'name', 'surname', 'profile_photo', 'description', 'phone_number']
         read_only_fields = fields
 
 
